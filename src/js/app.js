41bb893568cef9272acbbbad9abe3661cce743f4
Eric W. Greene <eric@ericwgreene.com>
2017-06-05 07:51:15 -0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import 'bootstrap-loader';
+import '../scss/styles.scss';
+
+class HelloWorld extends React.Component {
+
+    render() {
+        return <h1>Hello World!</h1>;    
+    }
+}
+
+ReactDOM.render(<HelloWorld />, document.querySelector('main'));
