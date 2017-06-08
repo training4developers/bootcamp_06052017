dd6c7d6fe379fe0263276cb56aa5b520b01a8ca0
Eric W. Greene <eric@ericwgreene.com>
2017-06-08 11:35:25 -0700

    added code demos

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,10 @@ const colors = [
     'teal',
 ];
 
+const cars = [
+    { id: 1, make: 'Toyota', model: 'Camry', color:'blue', year: 2017, price: 20183 },
+];
+
 ReactDOM.render(
     <ColorTool headerText="Color Tool!" colorList={colors} />,
     document.querySelector('main'),
--- src/js/components/color-tool.component.js
+++ src/js/components/color-tool.component.js
@@ -2,12 +2,34 @@ import * as React from 'react';
 
 export class ColorTool extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newColor: '',
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            [ e.target.name ]: e.target.value,
+        });
+    }
+
     render() {
         return <div>
             <h1>{this.props.headerText}</h1>
             <ul>
                 {this.props.colorList.map( color => <li>{color}</li> )}
             </ul>
+            <form>
+                <label htmlFor="new-color-input">New Color</label>
+                <input type="text" id="new-color-input"
+                    name="newColor"
+                    value={this.state.newColor} onChange={this.onChange} />
+            </form>
         </div>;
     }
 }
