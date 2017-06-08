c630a34df4ee49d7017ad355df6ff4b708ed040e
Eric W. Greene <eric@ericwgreene.com>
2017-06-08 13:47:05 -0700

    added code demos

--- src/js/components/color-tool.component.js
+++ src/js/components/color-tool.component.js
@@ -6,10 +6,12 @@ export class ColorTool extends React.Component {
         super(props);
 
         this.state = {
+            colorList: this.props.colorList.concat(),
             newColor: '',
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
 
     onChange(e) {
@@ -18,17 +20,27 @@ export class ColorTool extends React.Component {
         });
     }
 
+    onClick() {
+
+        this.setState({
+            colorList: this.state.colorList.concat(this.state.newColor),
+            newColor: '',
+        });
+
+    }
+
     render() {
         return <div>
             <h1>{this.props.headerText}</h1>
             <ul>
-                {this.props.colorList.map( color => <li>{color}</li> )}
+                {this.state.colorList.map( color => <li>{color}</li> )}
             </ul>
             <form>
                 <label htmlFor="new-color-input">New Color</label>
                 <input type="text" id="new-color-input"
                     name="newColor"
                     value={this.state.newColor} onChange={this.onChange} />
+                <button type="button" onClick={this.onClick}>Add Color</button>
             </form>
         </div>;
     }
